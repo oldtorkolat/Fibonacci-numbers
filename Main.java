4b6a9aff622687e964e2c582a448023aa0bf9fdf
vadimati <torkolatvadim@gmail.com>
2015-04-19 14:43:33 +0300

    final realise

---
+++ Main.java
@@ -0,0 +1,37 @@
+/**
+ * Рассматривая условия в последовательности Фибоначчи, значения не превышают четыре миллиона найти сумму четныхзначных чисел.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int first = 1;
+        int second = 2;
+        int sum = 0;
+        int var = 0;
+
+        while(first+second <= 4_000_000)
+        {
+            var = first + second;
+            System.out.print(var + " , ");
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if(var % 2 == 0)
+                sum+=var;
+            first = second;
+            second = var;
+
+        }
+        System.out.println("\nThe sum of even-valued terms is :"+sum);
+
+
+    }
+}
+
+
+
+
+
+
+
